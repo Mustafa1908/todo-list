1ef7f675cdcbfd852a0e2063d30fc3f49cc68d31
Mustafa Özkara <mustafaozkara@outlook.fr>
2024-02-15 16:16:02 +0100

    Move showProjects module here to be able to get the dataset-index and acces the task of each project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 import addProjects from "./addProjects";
+import CreateToDoTask from "./createTodoTask";
+import getProjectIndexValue from "./getProjectIndexValue";
 import "./styles.css"
 
+const user = new CreateToDoTask("mustafa");
+let projectIndexValue = -1;
 
 document.addEventListener("click", (e) => {
     const target = e.target.closest(".add-project"); 
-  
     if(target){
       addProjects();
+      user.createTaskArray();
+      user.showArray();
+      projectIndexValue++;
     }
   });
 
+
+
+  document.addEventListener("click", (e) => {
+    const target = e.target.closest(".add-task"); 
+  
+    if(target){
+      let userTask = prompt("Enter a task");
+      user.addTaskToArray(projectIndexValue, userTask);
+      user.showArray();
+    }
+  });
+
+
+
+  
+    
+  function showProjects(projectName, projectDescription) {
+  projectName.addEventListener('click', e => {
+      let projectDisplayName = document.querySelector(".project-name");
+      let projectDisplayDescription = document.querySelector(".project-description");
+      var elem = e.target.closest("[class]");
+      if (elem && elem.getAttribute('class') !== 'root') {
+      }  projectDisplayName.innerHTML = `<span class="project-name">${projectName.innerText}</span>`;
+      projectDisplayDescription.innerHTML = `<span class="project-description">${projectDescription}</span>`;
+  });
+}
+
+
+export default showProjects
