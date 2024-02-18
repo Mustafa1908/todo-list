044c5f26aff24f62401bdbb5f5fa31708ba073ee
Mustafa Özkara <mustafaozkara@outlook.fr>
2024-02-18 18:01:52 +0100

    Add function inside addtask eventlistener that show an input and then show it when submitting it, add input element when clicking on addtask and showing it when user submit it

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import addProjects from "./addProjects";
 import CreateToDoTask from "./createTodoTask";
 import getProjectIndexValue from "./getProjectIndexValue";
+import createInputForAddProject from "./createInputsForAddProject";
 import "./styles.css"
 
-const user = new CreateToDoTask("mustafa");
+
+
+const user = new CreateToDoTask("user");
 let projectIndexValue = -1;
+let currentProjectIndex = 0;
 
 document.addEventListener("click", (e) => {
     const target = e.target.closest(".add-project"); 
     if(target){
-      addProjects();
-      user.createTaskArray();
-      user.showArray();
-      projectIndexValue++;
+        createInputForAddProject()
+        user.createTaskArray();
+        alert("hey")
+        projectIndexValue++;
     }
   });
 
@@ -22,26 +25,56 @@ document.addEventListener("click", (e) => {
     const target = e.target.closest(".add-task"); 
   
     if(target){
-      let userTask = prompt("Enter a task");
-      user.addTaskToArray(projectIndexValue, userTask);
-      user.showArray();
+      let projectInputContainer = document.querySelector("#add-task-container");
+      let taskInput = document.createElement("input");
+      let submitTask = document.createElement("submit-task");
+
+      taskInput.innerHTML = `<input type="text">`
+      submitTask.innerHTML = `<button type="submit" id="submit-task">submit</button>`
+
+      projectInputContainer.appendChild(taskInput);
+      projectInputContainer.appendChild(submitTask);
+      taskInput.focus();
+      
+
+      document.addEventListener("click", (e) => {
+        const target = e.target.closest("#submit-task"); 
+        if(target){
+          e.preventDefault();
+          if (taskInput.value === "") {
+            return;
+          }
+          
+
+          //user.addTaskToArray(getProjectIndexValue(projectName), taskInput.value);
+          user.showArray();
+          taskInput.value = ""
+          
+          taskInput.remove();
+          submitTask.remove();
+        }
+      });
     }
   });
 
-
-
-  
     
   function showProjects(projectName, projectDescription) {
   projectName.addEventListener('click', e => {
+      let projectIndex = getProjectIndexValue(projectName);
+      let taskArrayLength = user.showTaskArrayLength(projectIndex);
+      user.renderToDoTask(projectIndex, taskArrayLength);
       let projectDisplayName = document.querySelector(".project-name");
       let projectDisplayDescription = document.querySelector(".project-description");
+
       var elem = e.target.closest("[class]");
+
       if (elem && elem.getAttribute('class') !== 'root') {
       }  projectDisplayName.innerHTML = `<span class="project-name">${projectName.innerText}</span>`;
       projectDisplayDescription.innerHTML = `<span class="project-description">${projectDescription}</span>`;
+      currentProjectIndex = projectIndex;
   });
 }
+
 
 
 export default showProjects
